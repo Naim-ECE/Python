deea37e643a24dfabe4603802b8ffc69fa6973b9
Md. Naimur Rahman Naim <naimur2210026@gmail.com>
2026-07-24 17:00:28 +0600

    python basic play

---
+++ 1-Basic/module.py
@@ -0,0 +1,10 @@
+import pyjokes
+
+# for install pyjokes 'pip install pyjokes'
+# the code imports the pyjokes library, which is a Python library for generating programming jokes. The line `joke = pyjokes.get_joke()` retrieves a random joke from the library, and `print(joke)` outputs the joke to the console.
+"""
+multiline comments
+"""
+
+joke = pyjokes.get_joke()
+print(joke)
